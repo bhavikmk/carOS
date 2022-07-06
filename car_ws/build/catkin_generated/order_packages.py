e06d5e1a20bd64fae8786d4df6157f48837347e9
bhavikmk <u19me151@med.svnit.ac.in>
2022-07-07 00:05:08 +0530

    Add package structure

---
+++ car_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/bhavik/projects/car/AutoCar/car_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/bhavik/amr_ws/devel;/home/bhavik/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/bhavik/amr_ws/devel;/home/bhavik/catkin_ws/devel;/opt/ros/noetic' != '' else []
