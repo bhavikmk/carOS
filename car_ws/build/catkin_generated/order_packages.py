c9069ecee3af88ba2dd8041633b17fd1d9289a27
bhavikmk <u19me151@med.svnit.ac.in>
2022-07-19 22:33:36 +0530

    Remove build and devel

--- car_ws/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/bhavik/projects/car/autoCar/car_ws/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/bhavik/amr_ws/devel;/home/bhavik/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/bhavik/amr_ws/devel;/home/bhavik/catkin_ws/devel;/opt/ros/noetic' != '' else []
---
+++ car_ws/src/planning/behavioral_planner/README.md
@@ -0,0 +1,9 @@
+# Behavioral planning
+
+## Description
+
+Behavioral planning is selecting driving behaviour based on environment state and vehicle state. Traffic rules, static and dynamic objects are considered in behaviour planning.
+
+It can be modelled as finite state machines and markov decision processes.
+
+
